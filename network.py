c53fdea2b57c192b8974a366d0163a3d4302504b
robert.pottorff <rpottorff@gmail.com>
2016-06-06 11:38:00 -0600

    bleh

--- network.py
+++ network.py
@@ -4,142 +4,12 @@ import os
 import numpy as np
 import shutil
 
-class TrainTarget(object):
-    def __init__(self, Type, args, environment):
-        self.args = args
-        self.sess = Type.create_session(args)
 
-        self.target_network = Type(args, environment, 'target', self.sess)
-        self.train_network = Type(args, environment, 'train', self.sess)
-
-        self.training_iterations = 0
-        self.lr = 0.0
-        self.batch_loss = 0.0
-
-        with tf.name_scope(args.network_type + "-update") as scope:
-            self.copy = [
-                weight.assign(args.target_network_alpha * self.train_network.weights[i] + (1.0 - args.target_network_alpha) * weight, use_locking=True)
-                    for i, weight in enumerate(self.target_network.weights)
-            ] + [
-                bias.assign(args.target_network_alpha * self.train_network.biases[i] + (1.0 - args.target_network_alpha) * bias, use_locking=True)
-                    for i, bias in enumerate(self.target_network.biases)
-            ]
-
-        self.tensorboard()
-
-    def __len__(self):
-        return len(self.train_network)
-
-    def tensorboard(self):
-        tf.train.SummaryWriter(self.args.tf_summary_path, self.sess.graph)
-        subprocess.Popen(["tensorboard", "--logdir=" + self.args.tf_summary_path],
-                         stdout=open(os.devnull, 'w'),
-                         stderr=open(os.devnull, 'w'),
-                         close_fds=True)
-
-    def update(self):
-        self.sess.run(self.copy)
-
-    def q(self, states):
-        return self.target_network.q(states)
-
-    def train(self, states, actions, terminals, next_states, rewards, lookahead=None):
-        response = self.train_network.train(states, actions, terminals,
-                                            next_states, rewards, lookahead, self.target_network)
-        self.training_iterations = self.train_network.training_iterations
-        self.lr = self.train_network.lr
-        self.batch_loss = self.train_network.batch_loss
-
-        if self.training_iterations % self.args.copy_frequency == 0:
-            self.update()
-
-        return response
-
-
-class Network(object):
-    def __init__(self, args, environment, name='network', sess=None):
+class Network:
+    def __init__(self, args, environment):
         self.args = args
         self.environment = environment
-        self.name = name
-        self.training_iterations = 0
-        self.batch_loss = 0
-        self.lr = 0
         self.default_initializer = args.initializer
-
-        self.weights = []
-        self.biases = []
-        self.activations = []
-        self.additional_q_ops = []
-
-        self.sess = self.create_session(args) if sess is None else sess
-
-        with tf.device('/cpu:0'):
-            self.global_step = tf.Variable(0, name='global_step', trainable=False)
-
-        with tf.name_scope('input') as scope:
-            self.state = self.float([None, args.phi_frames] + list(environment.get_state_space()), name='state') / float(environment.max_state_value())
-            self.lookahead = self.float([None, args.phi_frames] + list(environment.get_state_space()), name='lookahead') / float(environment.max_state_value())
-            self.next_q = self.float([None], name='next_q')
-            self.action = self.int([None], name='action_index')
-            self.action_one_hot = self.one_hot(self.action, self.environment.get_num_actions(), name='one_hot_action')
-            self.terminal = self.float([None], name='terminal')
-            self.reward = self.float([None], name='reward')
-            self.processed_reward = tf.clip_by_value(self.reward, -1.0, 1.0) if self.args.clip_reward else self.reward
-
-        self.output = None
-
-    def __len__(self):
-        return sum([sum([reduce(lambda x, y: x * y, l.get_shape().as_list()) for l in e]) for e in [self.weights, self.biases]])
-
-    def get_loss(self, processed_delta, prediction, truth):
-        return tf.reduce_mean(tf.square(processed_delta), name='loss')
-
-    def post_init(self):
-        with tf.name_scope('output') as scope:
-            self.q_action = self.argmax(self.output)
-            self.q_max = self.max(self.output)
-
-        with tf.name_scope('target_q') as scope:
-            target_q = self.processed_reward + self.args.discount * (1.0 - self.terminal) * self.next_q
-
-        with tf.name_scope('delta') as scope:
-            q_acted = self.sum(self.output * self.action_one_hot, name='q_acted')
-            self.delta = target_q - q_acted
-            self.processed_delta = tf.clip_by_value(self.delta, -1.0, 1.0) if self.args.clip_tderror else self.delta
-
-        with tf.name_scope('loss') as scope:
-            self.loss = self.get_loss(self.processed_delta, truth=target_q, prediction=q_acted)
-
-        with tf.name_scope('learning_rate') as scope:
-            # Learning Rate Calculation
-            self.learning_rate = tf.maximum(self.args.learning_rate_end,
-                                            tf.train.exponential_decay(
-                                              self.args.learning_rate_start,
-                                              self.global_step,
-                                              self.args.learning_rate_decay_step,
-                                              self.args.learning_rate_decay,
-                                              staircase=False))
-
-        with tf.name_scope('optimizer') as scope:
-            self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate,
-                                                       decay=self.args.rms_decay,
-                                                       momentum=float(self.args.rms_momentum),
-                                                       epsilon=self.args.rms_eps).minimize(self.loss,
-                                                                                           global_step=self.global_step,
-                                                                                           colocate_gradients_with_ops=True)
-
-        # Initialize
-
-        with tf.device('/cpu:0'):
-            self.sess.run(tf.initialize_all_variables())
-
-    @staticmethod
-    def create_session(args):
-        tf.set_random_seed(args.random_seed)
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_fraction,
-                                    allow_growth=True)
-        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,
-                                                allow_soft_placement=True))
 
     def one_hot(self, source, size, name='onehot'):
         return tf.one_hot(source, size, 1.0, 0.0, name=name)
@@ -152,7 +22,7 @@ class Network(object):
 
     def flatten(self, source, name='flatten'):
         shape = source.get_shape().as_list()
-        dim = reduce(lambda x, y: x*y, shape[1:])
+        dim = reduce(lambda x, y: x * y, shape[1:])
 
         with tf.variable_scope(name + "_flatten"):
             return tf.reshape(source, [-1, dim], name=name)
@@ -165,11 +35,8 @@ class Network(object):
         with tf.variable_scope(name + "_merge"):
             return tf.concat(idx, [left, right])
 
-    def sum(self, source, name, idx=1):
+    def sum(self, source, name=None, idx=1):
         return tf.reduce_sum(source, reduction_indices=idx, name=name)
-
-    def squared_sum(self, source, name):
-        return tf.reduce_sum(tf.square(source), reduction_indices=0, name=name)
 
     def parse_initializer(self, initializer, stddev):
         return {
@@ -185,7 +52,8 @@ class Network(object):
             'sigmoid': tf.nn.sigmoid,
             'none': None}[activation]
 
-    def linear(self, source, output_size, stddev=0.02, initializer='default', bias_start=0.01, activation_fn='relu', name='linear', w=None, b=None):
+    def linear(self, source, output_size, stddev=0.02, initializer='default', bias_start=0.01, activation_fn='relu',
+               name='linear', w=None, b=None):
         shape = source.get_shape().as_list()
 
         initializer = self.parse_initializer(initializer, stddev)
@@ -194,14 +62,11 @@ class Network(object):
         with tf.variable_scope(name + '_linear') as scope:
             if w is None:
                 w = tf.get_variable("weight", [shape[1], output_size], tf.float32, initializer)
-                self.weights.append(w)
             if b is None:
                 b = tf.get_variable("bias", [output_size], initializer=tf.constant_initializer(bias_start))
-                self.biases.append(b)
 
             out = tf.nn.bias_add(tf.matmul(source, w), b)
             activated = activation_fn(out) if activation_fn is not None else out
-            self.activations.append(activated)
 
             return activated, w, b
 
@@ -214,15 +79,12 @@ class Network(object):
         with tf.variable_scope(name + '_conv2d') as scope:
             if w is None:
                 w = tf.get_variable("weight", shape=[size, size, shape[1], filters], initializer=initializer)
-                self.weights.append(w)
             if b is None:
-                b = tf.Variable(tf.constant(bias_start, shape=[filters]), name="bias")
-                self.biases.append(b)
+                b = tf.get_variable("bias", [filters], initializer=tf.constant_initializer(bias_start))
 
             c = tf.nn.conv2d(source, w, strides=[1, 1, stride, stride], padding=padding, data_format='NCHW')
             out = tf.nn.bias_add(c, b, data_format='NCHW')
             activated = activation_fn(out) if activation_fn is not None else out
-            self.activations.append(activated)
 
             return activated, w, b
 
@@ -232,42 +94,173 @@ class Network(object):
     def int(self, shape, name='int'):
         return tf.placeholder('int64', shape, name=name)
 
-    def q(self, states):
-        return self.sess.run([self.q_action, self.output] + self.additional_q_ops, feed_dict={self.state: states})
+    def loss(self, delta):
+        return tf.reduce_mean(tf.square(delta), name='loss')
 
-    def train(self, states, actions, terminals, next_states, rewards, lookahead=None, target_network=None):
+    def optimizer(self, learning_rate):
+        return tf.train.RMSPropOptimizer(learning_rate=learning_rate,
+                                         decay=self.args.rms_decay,
+                                         momentum=float(self.args.rms_momentum),
+                                         epsilon=self.args.rms_eps)
 
-        network = self if target_network is None else target_network
+class Commander(Network):
+    def __init__(self, Type, args, environment):
+        Network.__init__(self, args, environment)
+
+        self.training_iterations = 0
+        self.batch_loss = 0
+        self.lr = 0
+        gradients = []
+
+        self.sess = self.start_session(args)
+        network = Type(args, environment)
+
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
+        optimizer = network.optimizer(learning_rate=self.args.learning_rate_end)
+
+        with tf.name_scope('inputs') as _:
+            self.states = self.float([None, args.phi_frames] + list(environment.get_state_space()), name='state')
+            self.next_states = self.float([None, args.phi_frames] + list(environment.get_state_space()), name='state')
+            self.actions = self.int([None], name='action_index')
+            self.terminals = self.float([None], name='terminal')
+            self.rewards = self.float([None], name='reward')
+
+        with tf.name_scope('split_inputs') as _:
+            assert self.args.batch_size % self.args.threads == 0, "Error: Threads must divide batch_size evenly"
+            states = tf.split(split_dim=0, num_split=self.args.threads, value=self.states)
+            actions = tf.split(split_dim=0, num_split=self.args.threads, value=self.actions)
+            terminals = tf.split(split_dim=0, num_split=self.args.threads, value=self.terminals)
+            next_states = tf.split(split_dim=0, num_split=self.args.threads, value=self.next_states)
+            rewards = tf.split(split_dim=0, num_split=self.args.threads, value=self.rewards)
+
+        for n in range(2):
+            with tf.device('/gpu:{}'.format(n)):
+                print "with device /gpu:{}".format(n)
+                with tf.name_scope('thread_{}'.format(n)):
+
+                    processed_rewards = tf.clip_by_value(rewards[n], -1.0, 1.0, name='clip_reward') if self.args.clip_reward else rewards[n]
+
+                    with tf.variable_scope('target_network', reuse=n > 0) as scope:
+                        next_qs = network.build(states=next_states[n] / float(environment.max_state_value()))
+                        next_best_qs = self.max(next_qs)
+
+                    with tf.variable_scope('train_network', reuse=n > 0) as scope:
+                        train_qs = network.build(states=states[n] / float(environment.max_state_value()))
+                        train_acted_qs = self.sum(train_qs * self.one_hot(actions[n], environment.get_num_actions()))
+
+                    with tf.name_scope('target_q'):
+                        target_q = tf.stop_gradient(processed_rewards + self.args.discount * (1 - terminals[n]) * next_best_qs)
+
+                    with tf.name_scope('delta'):
+                        delta = train_acted_qs - target_q
+                        processed_delta = tf.clip_by_value(delta, -1.0, 1.0) if self.args.clip_tderror else delta
+
+                    with tf.name_scope('loss'):
+                        loss = network.loss(processed_delta)
+
+                    gradient = optimizer.compute_gradients(loss, colocate_gradients_with_ops=True)
+                    gradients.append(gradient)
+
+        with tf.name_scope('thread_actor'):
+            with tf.variable_scope('target_network', reuse=True) as _:
+                self.qs = network.build(states=self.states)
+                self.qs_argmax = self.argmax(self.qs)
+
+        self.target_vars = tf.get_collection(tf.GraphKeys.VARIABLES, scope='target')
+        self.train_vars = tf.get_collection(tf.GraphKeys.VARIABLES, scope='train')
+
+        with tf.name_scope('copy'):
+            self.assign_ops = [target.assign(train) for target, train in zip(self.target_vars, self.train_vars)]
+
+        gradient = self.average_gradients(gradients)
+        self.train_op = optimizer.apply_gradients(gradient, global_step=self.global_step)
+
+        init_op = tf.initialize_all_variables()
+
+        self.sess.run(init_op)
+        self.tensorboard()
+
+    def __len__(self):
+        return sum([sum([reduce(lambda x, y: x * y, l.get_shape().as_list()) for l in e]) for e in [self.train_vars]])
+
+    def start_session(self, args):
+        tf.set_random_seed(args.random_seed)
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_fraction,
+                                    allow_growth=True)
+        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,
+                                                allow_soft_placement=True,
+                                                log_device_placement=True))
+
+    def tensorboard(self):
+        tf.train.SummaryWriter(self.args.tf_summary_path, self.sess.graph)
+        subprocess.Popen(["tensorboard", "--logdir=" + self.args.tf_summary_path],
+                         stdout=open(os.devnull, 'w'),
+                         stderr=open(os.devnull, 'w'),
+                         close_fds=True)
+
+    def update(self):
+        self.sess.run(self.assign_ops)
+
+    def train(self, states, actions, terminals, next_states, rewards, lookahead=None):
 
         data = {
-            self.state: states,
-            self.action: actions,
-            self.terminal: terminals,
-            self.reward: rewards,
-            self.next_q: self.sess.run(network.q_max, feed_dict={network.state: next_states})
+            self.states: states,
+            self.actions: actions,
+            self.terminals: terminals,
+            self.rewards: rewards,
+            self.next_states: next_states
         }
 
-        if lookahead is not None:
-            data[self.lookahead] = lookahead
+        _, self.training_iterations = self.sess.run([self.train_op, self.global_step], feed_dict=data)
 
-        _, self.training_iterations, delta, self.batch_loss, self.lr = self.sess.run(
-            [self.optimizer, self.global_step, self.delta, self.loss, self.learning_rate], feed_dict=data)
+        if self.training_iterations % self.args.copy_frequency == 0:
+            self.update()
 
-        return delta, self.batch_loss
+    def q(self, states):
+        return self.sess.run([self.qs_argmax, self.qs], feed_dict={self.states: states})
+
+    def average_gradients(self, tower_grads):
+        with tf.name_scope('average_gradients'):
+
+            average_grads = []
+            for grad_and_vars in zip(*tower_grads):
+
+                # Note that each grad_and_vars looks like the following:
+                #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+                grads = []
+                for gradient, variable in grad_and_vars:
+                    if gradient is not None:
+                        expanded_g = tf.expand_dims(gradient, 0) # add tower dim
+                        grads.append(expanded_g)
+
+                if len(grads) > 0:
+                    # Average over the 'tower' dimension.
+                    grad = tf.concat(0, grads)
+                    grad = tf.reduce_mean(grad, 0)
+
+                    # Keep in mind that the Variables are redundant because they are shared
+                    # across towers. So .. we will just return the first tower's pointer to
+                    # the Variable.
+                    v = grad_and_vars[0][1]
+                    grad_and_var = (grad, v)
+                    average_grads.append(grad_and_var)
+            return average_grads
+
+
 
 class Baseline(Network):
-    def __init__(self, args, environment, name='network', sess=None):
-        with tf.variable_scope("baseline-" + name) as scope:
-            Network.__init__(self, args, environment, name, sess)
+    def __init__(self, args, environment):
+        Network.__init__(self, args, environment)
 
-            # Build Network
-            self.conv1,  w1, b1 = self.conv2d(self.state, size=8, filters=32, stride=4, name='conv1')
-            self.conv2,  w2, b2 = self.conv2d(self.conv1, size=4, filters=64, stride=2, name='conv2')
-            self.conv3,  w3, b3 = self.conv2d(self.conv2, size=3, filters=64, stride=1, name='conv3')
-            self.fc4,    w4, b4 = self.linear(self.flatten(self.conv3, name="fc4"), 512, name='fc4')
-            self.output, w5, b5 = self.linear(self.fc4, environment.get_num_actions(), activation_fn='none', name='output')
+    def build(self, states):
+        # Build Network
+        conv1, w1, b1 = self.conv2d(states, size=8, filters=32, stride=4, name='conv1')
+        conv2, w2, b2 = self.conv2d(conv1, size=4, filters=64, stride=2, name='conv2')
+        conv3, w3, b3 = self.conv2d(conv2, size=3, filters=64, stride=1, name='conv3')
+        fc4, w4, b4 = self.linear(self.flatten(conv3, name="fc4"), 512, name='fc4')
+        output, w5, b5 = self.linear(fc4, self.environment.get_num_actions(), activation_fn='none', name='output')
 
-            self.post_init()
+        return output
 
 
 class Linear(Network):
@@ -280,6 +273,7 @@ class Linear(Network):
             self.output, w2, b2 = self.linear(self.fc2, environment.get_num_actions(), activation_fn='none', name='output')
 
             self.post_init()
+
 
 class Constrained(Network):
     def __init__(self, args, environment, name='network', sess=None):
